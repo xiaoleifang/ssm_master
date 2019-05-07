7d433010e1f3d65a1c55155b64942851132e540e
xiaoleifang <2747857230@qq.com>
2019-05-07 15:56:11 +0800

    开发了一个app类

--- src/main/java/com/oracle/web/test/TestApp2.java
+++ src/main/java/com/oracle/web/test/TestApp2.java
@@ -1,11 +1,11 @@
 package com.oracle.web.test;
 
-public class TestWeb {
+public class TestApp2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("hello");
+		System.out.println("right....");
 	}
 
 }
